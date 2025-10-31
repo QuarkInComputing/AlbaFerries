bbd5aef90268b4478f1154380958e968c7ed3cef
Jack McLean-Wilson <jackmcleanwilson@gmail.com>
2025-10-31 11:25:17 +0000

    Added existing website files

---
+++ js/navigation.js
@@ -0,0 +1,7 @@
+const nav = document.getElementsByClassName('navlinks')[0];
+
+let nav1 = '<li><a class="navlink" href="index.html">Book</a></li>';
+let nav2 = '<li><a class="navlink" href="about.html">About</a></li>';
+let nav3 = '<li><a class="navlink" href="contact.html">Contact</a></li>';
+
+nav.innerHTML = nav1+nav2+nav3
